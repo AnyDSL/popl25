a45c319a145998ec628aeb0f8cbe622888e92f30
Joachim Meyer <jmeyer@cs.uni-saarland.de>
2024-10-24 15:40:54 +0200

    Fix-up bootstrap.sh.
    Add PPA for newer gcc, if on older Ubuntu.
    Install missing packages and fix typo in setup env script.

--- vm/bootstrap.sh
+++ vm/bootstrap.sh
@@ -2,13 +2,18 @@
 
 set -ex
 
+# check if ubuntu version is older than 24.04, in that case add PPA:
+if [[ $(grep "DISTRIB_RELEASE=" /etc/lsb-release | grep -oP "\d\d.\d\d") < "24.04" ]]; then
+    add-apt-repository ppa:ubuntu-toolchain-r/ppa
+fi
+
 apt-get update
 apt-get install -y vim tmux htop nano
 apt-get install -y python3 python3-pip python3-dev python3-venv
-apt-get install -y gcc-14 g++-14
+apt-get install -y gcc-14 g++-14 libgmp-dev
 apt-get install -y llvm-18 clang-18
 apt-get install -y opam
-apt-get install -y libpcre2-dev libpcre3-dev
+apt-get install -y libpcre2-dev libpcre3-dev cloc
 
 ln -s /bin/clang-18 /bin/clang
 ln -s /bin/clang++-18 /bin/clang++
@@ -21,7 +26,7 @@ git clone https://github.com/AnyDSL/popl25.git ${AE_HOME}/popl25
 cd ${AE_HOME}/popl25
 
 cd vm
-./setup_venv.sh
+./setup-env.sh
 
 source ./env/bin/activate
 
